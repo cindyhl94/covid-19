e1e1c85ce2b59229484f6db97f1e860dddd7c26c
cindyhl94 <62523194+cindyhl94@users.noreply.github.com>
2020-05-19 21:34:04 +1000

    Uploaded config.py as test

---
+++ config.py
@@ -0,0 +1,12 @@
+model_save_path = "models/"
+
+datasets_path = {
+
+    "biorxiv_medrxiv" : "datasets/2020-03-13/biorxiv_medrxiv/biorxiv_medrxiv/",
+    "pmc_custom_license": "datasets/2020-03-13/pmc_custom_license/pmc_custom_license/",
+    "comm_use_subset" : "datasets/2020-03-13/comm_use_subset/comm_use_subset/",
+    "noncomm_use_subset" : "datasets/2020-03-13/noncomm_use_subset/noncomm_use_subset/"
+}
+
+
+
